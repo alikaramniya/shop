671aeb30da6f3f8825ae77aedbaeb24d124cf4a6
alikaram <alikaramniyabadreh98@gmail.com>
2020-04-06 17:56:14 +0430

    Move folders in the admin panel to mvc format

---
+++ admin/controller/Cindex.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once "model/Mindex.php";
+
+
+require_once "view/$controller/V$action.php";
+
+?>
---
+++ admin/view/index/Vindex.php
@@ -0,0 +1,13 @@
+<aside class="profile-nav col-lg-12">
+
+    <section class="panel">
+        <div class="user-heading round">
+            <a href="https://github.com/alikaramniya" target="_blank">
+                <img src="public/img/admin/ali.jpg" alt="">
+            </a>
+            <h1>Ali Karamniya</h1>
+            <p>alikaramniyabadreh98@gamil.com</p>
+            <p>im the best developer in the world</p>
+        </div>
+    </section>
+</aside>
