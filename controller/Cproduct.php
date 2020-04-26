9cd223e741b9bd7e5d0148c89ff9f771fc2bb890
alikaram <alikaramniyabadreh98@gmail.com>
2020-04-26 14:21:07 +0430

    Add product list and product details to the product controller

--- controller/Cproduct.php
+++ controller/Cproduct.php
@@ -5,12 +5,23 @@ require_once "admin/model/Mproduct.php";
 $pro = new Product();
 switch ($action) {
     case 'list':
-        $listProDefault = $pro->listPro();
+		$subCat = 'no';
+		$listProDefault = $pro->listPro();
+		if (isset($_GET['id'])) {
+			$subCat = 'yes';
+			$id = $_GET['id'];
+			$subCat = $pro->showSubCat($id);
+			$mainCat = $pro->showMainChid($subCat->chid);
+			$listProDefault = $pro->listProSubCat($id);
+		}
         break;
     case 'details':
         $id = $_GET['id'];
         $details = $pro->showEdit($id);
-        break;
+		$subCat = $pro->showSubCat($details->cat_id);
+		$mainCat = $pro->showMainChid($subCat->chid);
+        $listProDefault = $pro->listProSubCat($details->cat_id);
+		break;
 }
 
 require_once "view/$controller/V$action.php";
