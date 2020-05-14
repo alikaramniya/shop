85850217ab886069e1e8baed4f2d637d3cb2b46b
alikaram <alikaramniyabadreh98@gmail.com>
2020-05-14 10:38:10 +0430

    Link the basket model page to the layout.php file

--- controller/Clayout.php
+++ controller/Clayout.php
@@ -1,11 +1,27 @@
 <?php
 
+//procat
 require_once "admin/model/Mprocat.php";
 $procat = new Procat();
 $listMainProcat = $procat->listMainChid(['1', '0']);
 
+//product
+require_once "admin/model/Mproduct.php";
+$pro = new Product();
+$listPro = $pro->listPro();
+
+//menu
 require_once "admin/model/Mmenu.php";
 $menu = new Menu();
 $listMainMenu = $menu->listMainchid(['1', '0']);
+
+//basket
+require_once "admin//model/Mbasket.php";
+$basket = new Basket();
+if (isset($_SESSION['user_id'])) {
+    $listBasket = $basket->listBasket($_SESSION['user_id']);
+}
+
+
 
 ?>
